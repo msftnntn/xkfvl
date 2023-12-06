c6b0df0a9ad766e6e2adb75da114a127d1b847bb
msftnntn <152199184+msftnntn@users.noreply.github.com>
2023-12-06 23:54:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 function setup() {
-    createCanvas(windowWidth, windowHeight, SVG);
-}
+    createCanvas(windowWidth, windowHeight);
+  }
 
-function keyPressed(event){ // 엔터키로 저장하는 법
-    console.log(event.key);
-    if( event.key === "Enter") {
-        save("제목","svg")
-    }
-}
-
-function draw() {
-  
+  function draw() {
     background(255);
+    drawingText();
+  }
 
-    fill(7,105,0, 95);
-    noStroke();
-    ellipse(495,600,240,160); // 연한 잎
+  function drawingText() {
+    let textdraw = "皮";
+    let textsize = 52;
+    let opacity = 255;
 
 
-}
+    let X = width / 2 - textWidth(textdraw) / 2;
+    let Y = height / 2 + textsize / 3;
+
+    fill(0, opacity);
+    textSize(textsize);
+
+    for (let i = 0; i < 5; i++) {
+      let oX = random(-5, 5);
+      let oY = random(-5, 5);
+      text(textdraw, X + oX, Y + oY);
+    }
+  }
